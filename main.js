b75d0edc3bc70ce6790d724408bc990a8a516eda
modehans <monicaglezsanch@gmail.com>
2022-11-20 19:17:19 +0100

    Create functions to create balls that are eliminated by touching them and a function that counts the hits.

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const gameScreem = document.querySelector('.js_gameScreen');
+const counterHtml = document.querySelector('.js_counter');
+let counter = 0;
+
+const randomNum = (max) => {
+  return Math.ceil(Math.random() * max);
+};
+
+const upCounter = () => {
+  counter = counter + 1;
+  console.log(counter);
+  counterHtml.innerText = 'Puntos: ' + counter;
+};
+const removeBall = (ev) => {
+  gameScreem.removeChild(ev.target);
+};
+
+const handleClickBall = (ev) => {
+  upCounter();
+  removeBall(ev);
+};
+
+const createBall = () => {
+  let ball = document.createElement('div');
+  ball.className = 'ball';
+  ball.style.backgroundColor =
+    'rgb(' + randomNum(255) + ',' + randomNum(255) + ',' + randomNum(255) + ')';
+  ball.style.top = randomNum(80) + 'vh';
+  ball.style.left = randomNum(80) + 'vw';
+  ball.addEventListener('click', handleClickBall);
+  ball.addEventListener('animationend', function (ev) {
+    gameScreem.removeChild(ev.target);
+  });
+  gameScreem.appendChild(ball);
+};
+
+setInterval(createBall, 300);
